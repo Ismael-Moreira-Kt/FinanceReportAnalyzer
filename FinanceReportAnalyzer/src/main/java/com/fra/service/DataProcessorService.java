f0670ddfa1b297d023e72703842764f498a3ef1a
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-25 17:46:48 +0100

    feat: Addition of the function that filters the results.

---
+++ FinanceReportAnalyzer/src/main/java/com/fra/service/DataProcessorService.java
@@ -0,0 +1,19 @@
+package com.fra.service;
+
+
+
+import java.util.List;
+
+import com.fra.model.Report;
+import com.google.common.collect.Lists;
+
+
+
+public class DataProcessorService {
+    public List<Report> filteReports(List<Report> reports) {
+        List<Report> filteredReports = Lists.newArrayList();
+        filteredReports.addAll(reports);
+        
+        return filteReports(reports);
+    }
+}
