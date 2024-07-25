bae0ed2427902cd2a8e142610c14dd53a2f64c1c
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-25 17:16:04 +0100

    feat: Addition of selenium options configuration.

--- FinanceReportAnalyzer/src/main/java/com/fra/service/DataExtractorService.java
+++ FinanceReportAnalyzer/src/main/java/com/fra/service/DataExtractorService.java
@@ -2,6 +2,25 @@ package com.fra.service;
 
 
 
+import com.fra.model.Report;
+
+import java.util.List;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+
 public class DataExtractorService {
     public static final String CHROME_DRIVER_PATH = "/home/ismael/bin/chromedriver";
+
+
+    public List<Report> extracReports(String url) {
+        System.setProperty("webdirever.chrome.driver", CHROME_DRIVER_PATH)
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+    }
 }
