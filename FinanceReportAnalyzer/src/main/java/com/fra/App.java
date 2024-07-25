8fc89229adcd4d177259e1b274b3cd407c41d40b
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-25 16:42:18 +0100

    feat: Modification of App.java to include log configuration.

--- FinanceReportAnalyzer/src/main/java/com/fra/App.java
+++ FinanceReportAnalyzer/src/main/java/com/fra/App.java
@@ -1,13 +1,14 @@
 package com.fra;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+
+import com.fra.util.LoggerUtil;
+
+
+
+public class App {
+    public static void main(String[] args) {
+        LoggerUtil.configureLogger();
+        LoggerUtil.getLogger().info("Starting the Report Analyzer Application...");
     }
 }
