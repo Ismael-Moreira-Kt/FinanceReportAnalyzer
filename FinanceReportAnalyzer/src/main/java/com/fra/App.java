1b38c201fa0982c7307217bba283e4fa26da4de6
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-25 17:37:47 +0100

    feat: Addition of the DataExtractorService class to App.java.

--- FinanceReportAnalyzer/src/main/java/com/fra/App.java
+++ FinanceReportAnalyzer/src/main/java/com/fra/App.java
@@ -2,6 +2,11 @@ package com.fra;
 
 
 
+import java.io.IOException;
+import java.util.List;
+
+import com.fra.model.Report;
+import com.fra.service.DataExtractorService;
 import com.fra.util.LoggerUtil;
 
 
@@ -10,5 +15,16 @@ public class App {
     public static void main(String[] args) {
         LoggerUtil.configureLogger();
         LoggerUtil.getLogger().info("Starting the Report Analyzer Application...");
+    
+        String url = "https://finance.yahoo.com/";
+        String reportFilePath = "report.xlsx";
+    
+        DataExtractorService extractorService = new DataExtractorService();
+    
+        try {
+            List<Report> reports = extractorService.extracReports(url);
+        } catch(IOException exception) {
+            LoggerUtil.getLogger().error("Error processing reports", exception);
+        }
     }
 }
